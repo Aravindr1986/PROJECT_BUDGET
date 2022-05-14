1274bb2f70ef1d24580bdb0e8adaebc93141230c
Aravindr1986 <ramachandran.aravind@gmail.com>
2022-05-14 06:39:27 -0400

    Added config log change

--- src/config.js
+++ src/config.js
@@ -5,12 +5,11 @@ const mongoDBURL= "mongodb://localhost/BudgetData"
 async function startDatabase() {
     const connection = await MongoClient.connect(mongoDBURL, {useNewUrlParser: false});
     database = connection.db();
-    console.log("Dbconnected!")
+    console.log("Db connected!")
 }
 
 async function getDatabase() {
     if (!database)  startDatabase();
-    console.log(database)
     return database;
 }
 
