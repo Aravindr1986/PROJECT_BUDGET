7cb24359a494b3ed9e18c27ab28658896244502a
Aravindr1986 <ramachandran.aravind@gmail.com>
2022-05-22 11:32:51 -0400

    Added new route for getting total amount for dates

--- src/routes.js
+++ src/routes.js
@@ -25,15 +25,31 @@ router.get('/', async (req, res) => {
  *  post:
  *    description: Create a new expense
  *    responses:
- *     201:
+ *     200:
  *      description: Sucess response
  * 
  */
   
 router.post('/', async (req, res) => {
-    
+    req.body.expenseDate=new Date(req.body.expenseDate) ;
     response = await services.insertExpense(req.body)
     res.send(response);
   });
+  
+/**
+ * @swagger
+ * /totals:
+ *  get:
+ *    description: Retrieve total amount for an expense categories between the given start and end dates
+ *    responses:
+ *     200:
+ *      description: total amount for an expense categories between the given start and end dates
+ * 
+ */
+  
+router.get('/totals', async (req, res) => {  
+  response = await services.getTotals(new Date(req.body.startDate),new Date(req.body.endDate),req.body.expensType)
+  res.send(response);
+});
 
   module.exports = router;
--- src/services.js
+++ src/services.js
@@ -4,17 +4,31 @@ const collectionName = 'Budget';
 
 async function insertExpense(expenseItem) {
   const database = await getDatabase();
-  const {insertedExpense} = await database.collection(collectionName).insertOne(expenseItem);
+  const insertedExpense = await database.collection(collectionName).insertOne(expenseItem);
   return insertedExpense;
 }
 
 async function getExpense() {
-  
-  const database = await getDatabase();
+   const database = await getDatabase();
   return await database.collection(collectionName).find({}).toArray();
+}
+//{date: { $gte: startDate, $lt: endDate } },
+async function getTotals(startDate, endDate, expensType) {
+  const database = await getDatabase();
+  return await database.collection(collectionName).aggregate([{
+    $match : { $and : [ {expensType:{$eq: expensType}}] },
+  },{
+    $group : {
+        _id : null,
+        total : {
+            $sum : "$amount"
+        }
+    }
+  }]);
 }
 
 module.exports = {
   insertExpense,
   getExpense,
+  getTotals
 };
